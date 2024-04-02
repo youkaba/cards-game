d68ae2e46081ea48fbc85d7d73e8285ad0eec0c9
ykaba <youkaba2005@gmail.com>
2024-04-02 15:00:22 -0400

    Initial commit

---
+++ src/main/java/com/example/cardsgame/CardsGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.cardsgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cardsgame/CardsGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cardsgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
