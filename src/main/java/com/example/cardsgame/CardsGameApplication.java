11a8ae0e0608fa91faefc78194a40b1ee3ce6cf0
ykaba <youkaba2005@gmail.com>
2024-04-02 15:24:14 -0400

    config spring boot app

--- src/main/java/com/example/cardsgame/CardsGameApplication.java
+++ src/main/java/com/example/cardsgame/CardsGameApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CardsGameApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CardsGameApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CardsGameApplication.class, args);
+    }
 
 }
